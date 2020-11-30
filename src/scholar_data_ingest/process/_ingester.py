ac31ab832694d413cee6c2b9cb87b0712c2494bd
zushicat <karinhoehne@akasushi.de>
2020-11-30 09:53:36 +0100

    bulk import

--- src/scholar_data_ingest/process/_ingester.py
+++ src/scholar_data_ingest/process/_ingester.py
@@ -1,40 +1,107 @@
-import csv
-import datetime
 import json
 import os
 import logging
 from typing import Any, Dict, List, Optional, Tuple
+import uuid
 
-# from ._postgres_adapter import test
+from ._postgres_adapter import write_initial_DB
+from ldig import ldig
+
 
 _LOGGER = logging.getLogger(__package__)
-
 DIRNAME = os.environ["DATA_LOCATION"]
 
+ldig.check_loaded_model()
+
 
 # *******
-# write into DB (with _postgres_adapter methods)
+# write into DB tables (with _postgres_adapter methods)
 # *******
 def _write_table_text(data: Dict[str, Any]) -> None:
-    pass
+    is_written: bool = write_initial_DB("text", data, "paper_id")
     
 
 def _write_table_author(data: Dict[str, Any]) -> None:
-    pass
+    is_written: bool = write_initial_DB("author", data, "author_ids")
+    # _LOGGER.info(f"----> {is_written}")
 
 
 def _write_table_paper(data: Dict[str, Any]) -> None:
-    pass
+    is_written: bool = write_initial_DB("paper", data, "paper_id")
 
 
-def _set_db_entry(incoming: Dict[str, Any]) -> bool:
+# *******
+# initiate DB entries of line
+# *******
+def _set_db_entry(data: Dict[str, Any]) -> bool:
     try:
-        _LOGGER.info("---------> hello")
+        # ***
+        # check if entry is valid: MUST have a title and min. 1 author
+        # ***
+        if data["title"] is None or len(data["title"]) < 10:  
+            return False
+        if data["authors"] is None or len(data["authors"]) == 0:
+            return False
+
+        text_id: str = str(uuid.uuid4())
+
+        # ***
+        # check for language
+        # ***
+        check_title: str = data["title"]  # "私は猫で幸せです。"
+        check_abstract: str = data["paperAbstract"] if data["paperAbstract"] is not None else ""
+        detected_language, confidence = ldig.detect(f"{check_title} {check_abstract}")
+        
+        if confidence < 0.5:  # needs to be confident enough
+            detected_language = None
+        
+        # set empty abstract entry to None
+        data["paperAbstract"] = None if data["paperAbstract"] is None or len(data["paperAbstract"]) == 0 else data["paperAbstract"]
+
+        # ***
+        # process table text entry
+        # ***
+        _write_table_text({
+            "text_id": text_id,
+            "paper_id": data["id"],
+            "title": data["title"],
+            "abstract": data["paperAbstract"],
+            "language": detected_language
+        })
+
+        # ***
+        # process table author entries
+        # ***
+        all_author_ids = []  # collect ids
+        for author in data["authors"]:
+            all_author_ids += author["ids"]
+            _write_table_author({
+                "author_ids": ",".join(author["ids"]),
+                "name": author["name"]
+            })
+
+        # ***
+        # process table paper entry
+        # ***
+        _write_table_paper({
+            "paper_id": data["id"],
+            "year_published": data["year"],
+            "author_ids": ",".join(all_author_ids),
+            "research_fields": ",".join(data["fieldsOfStudy"]),
+            "text_id": text_id,
+            "is_cited_ids": ",".join(data["inCitations"]) if len(data["inCitations"]) > 0 else None,
+            "has_cited_ids": ",".join(data["outCitations"]) if len(data["outCitations"]) > 0 else None
+        })
+        
         return True
     except Exception as e:
+        _LOGGER.info(f"ERROR ---> {e}")
         return False
 
 
+# *******
+# process all incoming bulk data per line
+# *******
 def _process_bulk_file(filename: str) -> Tuple[Optional[int], Optional[int]]:
     '''
     Process lines of single bulk JSON line file (approx. 30000 lines).
@@ -52,7 +119,6 @@ def _process_bulk_file(filename: str) -> Tuple[Optional[int], Optional[int]]:
             except Exception as e:
                 # _LOGGER.info(f"ERROR 1 ---> {e}")
                 continue
-            break
         return count_written_ok, len(lines)
     except Exception as e:
         # _LOGGER.info(f"ERROR 0 ---> {e}")
@@ -63,4 +129,3 @@ def ingest_bulk(filenames: str) -> None:
     for filename in filenames:
         count_processed, count_all = _process_bulk_file(filename)
         _LOGGER.info(f"{count_processed}, {count_all}")
-        break
--- src/scholar_data_ingest/process/_postgres_adapter.py
+++ src/scholar_data_ingest/process/_postgres_adapter.py
@@ -4,6 +4,7 @@ from random import randrange
 from typing import Any, Dict, List, Set
 
 import psycopg2
+from psycopg2.extensions import AsIs  # https://stackoverflow.com/a/29471241
 
 
 _LOGGER = logging.getLogger(__package__)
@@ -15,11 +16,51 @@ POSTGRES_DB = os.environ.get("POSTGRES_DB")
 
 HOST = "host.docker.internal"  # quickfix
 
-conn = psycopg2.connect(
+CONNECTION = psycopg2.connect(
     f"port=5432 host={HOST} dbname={POSTGRES_DB} user={POSTGRES_USER} password={POSTGRES_PASSWORD}"
 )
-cur = conn.cursor()
 
 
-def test() -> None:
-    pass
+def write_initial_DB(table_name: str, data: Dict[str, Any], reference_id_name: str) -> bool:
+    '''
+    Generic insert statement from dict: https://stackoverflow.com/a/29471241
+    Check if reference_id is already in table. (Can be single or comma separated string.)
+    If existing: write entry (return True)
+    else: as of now just SKIP
+
+    TODO: make more efficient with copying data batch (https://www.dataquest.io/blog/loading-data-into-postgres/)
+    '''
+    cursor = CONNECTION.cursor()
+
+    # reference ids (here) are always strings
+    reference_id_value = data[reference_id_name]
+    sql_query = f"SELECT EXISTS(SELECT 1 FROM {table_name} WHERE {reference_id_name} = '{reference_id_value}');"
+    cursor.execute(sql_query)
+    is_existing: bool = cursor.fetchone()
+
+    # _LOGGER.info(f"{reference_id_value} {is_existing}")
+
+    columns = data.keys()
+    values = [data[column] for column in columns]
+    if True in is_existing:
+        # skip (usually some update mechanism would be useful)
+        return False
+    
+    # ***
+    # write new entry
+    # ***
+    try:
+        sql_query = f"INSERT INTO {table_name} (%s) VALUES %s;"
+
+        # _LOGGER.info(cursor.mogrify(sql_query, (AsIs(','.join(columns)), tuple(values))))
+        cursor.execute(sql_query, (AsIs(",".join(columns)), tuple(values)))
+        CONNECTION.commit()
+
+        return True
+    except Exception as e:
+        _LOGGER.info(f"SQL ERROR -----> {e}")
+
+        cursor.execute("ROLLBACK")
+        CONNECTION.commit()
+        return False
+
